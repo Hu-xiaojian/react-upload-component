0dee41ef0f6610b7621c55797552c58b382bf7a7
Hu-xiaojian <1759369551@qq.com>
2023-06-23 22:19:59 +0800

    perf: 包体积

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   resolve: {
@@ -35,5 +36,22 @@ module.exports = {
       }
     ]
   },
-
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'server',    //server | static | disabled
+      analyzerHost: '127.0.0.1',   // 默认值：127.0.0.1。 将在服务器模式下用于启动HTTP服务器的主机。
+      analyzerPort: 8889,         // 默认值：8888。将在服务器模式下用于启动HTTP服务器的端口。
+      reportFilename: 'report.html',      // 默认值：report.html。 捆绑将在静态模式下生成的报告文件的路径。 相对于bundle输出目录（在webpack配置中是output.path）。
+      defaultSizes: 'parsed',     // 默认值：已解析。 默认情况下在报告中显示的模块大小。 大小定义部分描述了这些值的含义。
+      openAnalyzer: true,     // 默认值：true。 在默认浏览器中自动打开报告。
+      generateStatsFile: false,   // 默认值：false。 如果为true，将在bundle输出目录中生成webpack stats JSON文件
+      statsFilename: 'stats.json',  // 默认值：stats.json。 如果generateStatsFile为true，将生成的webpack stats JSON文件的名称。 相对于bundle输出目录。
+      statsOptions: null,     // 默认值：null。 stats.toJson（）方法的选项。 例如，您可以使用source：false选项从stats文件中排除模块的源。 在这里查看更多选项。
+      logLevel: 'info'    // 默认值：info。 用于控制插件输出的详细信息。
+    }),
+  ],
+  externals: {
+    'react': 'var window.React',
+    'react-dom': 'var window.ReactDOM',
+  },
 }
--- demo/upload-dragger.jsx
+++ demo/upload-dragger.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Upload from '../src';
-
-import { Upload as AntdUpload } from 'antd';
-import { Upload as NextUpload } from '@alifd/next';
 import '../src/index.scss';
 
 const defaultValue = [
@@ -87,32 +84,6 @@ function UploadDraggable() {
       >
         {/*<div className='test'>拖拽</div>*/}
       </Upload.List>
-      <div>========================================================</div>
-      <AntdUpload
-        action='http://127.0.0.1:7001/file'
-        multiple onChange={e=> {
-        console.log(e,'-------------')
-      }}>
-        AntdUpload上传
-      </AntdUpload>
-      <div>========================================================</div>
-      <NextUpload
-        listType='text'
-        limit={10}
-        method={'post'}
-        action='http://127.0.0.1:7001/file'
-
-        // autoUpload
-        onError={(error, response, file) => {
-          console.log('------==========NextUpload-onError', error, response, file)
-        }}
-        onChange={value => {
-          // console.log(value,'----=============')
-        }}
-        headers={{'X-Requested-With':null}}
-      >
-        NextUpload
-      </NextUpload>
     </div>
   );
 }
